ebcae80354da1d72308fc38cae6a75b960dece8e
Imtiaz-Mahmood-Dev <imtiazdev@paklogics.com>
2025-07-03 22:32:53 +0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Apple on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
